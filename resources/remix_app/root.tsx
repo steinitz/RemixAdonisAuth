f3444a03b36df44a92c73ca842cc721921d11118
Steve Steinitz <steve@stzdev.com>
2024-09-16 14:47:43 +1000

    Fix AuthMiddleware to correctly identify protected routes. Add, possibly-redundant home page Add ReadMe.md Tweaks and minor fixes.

--- resources/remix_app/root.tsx
+++ resources/remix_app/root.tsx
@@ -10,6 +10,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
         {/* simple styling via MVP.css */}
         <link rel="stylesheet" href="https://unpkg.com/mvp.css" />
         <link rel="stylesheet" href="/resources/remix_app/mvp-css-override.css" />
+        {/* fonts */}
         <link
           rel="stylesheet"
           href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"
