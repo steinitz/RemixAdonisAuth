16fa56d4d25ec1cb8287c234b38244aaaa65f962
Steve Steinitz <steve@stzdev.com>
2024-09-19 17:30:57 +1000

    Add untested code to check for password-reset token expiry and show the user an expiry message

---
+++ resources/remix_app/components/styles.ts
@@ -0,0 +1,9 @@
+// hack to get email address and login/logout buttons to line up
+const loggedInButtonFormTopMarginTweak = '-21px'
+
+export const hideFormBorder: React.CSSProperties = {
+  border: "none",
+  boxShadow: "none",
+  textAlign: "right",
+  marginTop: loggedInButtonFormTopMarginTweak,
+}
