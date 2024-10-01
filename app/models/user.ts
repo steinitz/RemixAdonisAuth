429cf95dc9edaffa133e2fc65d7abad9ff939f32
Steve Steinitz <steve@stzdev.com>
2024-10-01 15:41:00 +1000

    database migration and User model update for email confirmation

--- app/models/user.ts
+++ app/models/user.ts
@@ -1,8 +1,8 @@
-import { DateTime } from 'luxon'
+import {DateTime} from 'luxon'
 import hash from '@adonisjs/core/services/hash'
-import { compose } from '@adonisjs/core/helpers'
-import { BaseModel, column } from '@adonisjs/lucid/orm'
-import { withAuthFinder } from '@adonisjs/auth/mixins/lucid'
+import {compose} from '@adonisjs/core/helpers'
+import {BaseModel, column} from '@adonisjs/lucid/orm'
+import {withAuthFinder} from '@adonisjs/auth/mixins/lucid'
 
 const AuthFinder = withAuthFinder(() => hash.use('scrypt'), {
   uids: ['email'],
@@ -10,27 +10,36 @@ const AuthFinder = withAuthFinder(() => hash.use('scrypt'), {
 })
 
 export default class User extends compose(BaseModel, AuthFinder) {
-  @column({ isPrimary: true })
+  @column({isPrimary: true})
   declare id: number
 
+  // not yet used
   @column()
   declare fullName: string | null
 
+  // login credentials
   @column()
   declare email: string
-
-  @column({ serializeAs: null })
+  @column({serializeAs: null})
   declare password: string | null
 
+  // password reset token
   @column()
   declare password_reset_token: string
-
-  @column.dateTime({ autoCreate: false })
+  @column.dateTime({autoCreate: false})
   declare password_reset_token_expiry: DateTime | null
 
-  @column.dateTime({ autoCreate: true })
-  declare createdAt: DateTime
+  // email confirmation token and status
+  @column()
+  declare email_confirmation_token: string
+  @column.dateTime({autoCreate: false})
+  declare email_confirmation_token_expiry: DateTime | null
+  @column()
+  declare is_email_confirmed: boolean
 
-  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  // created and updated timestamps
+  @column.dateTime({autoCreate: true})
+  declare createdAt: DateTime
+  @column.dateTime({autoCreate: true, autoUpdate: true})
   declare updatedAt: DateTime | null
 }
--- database/migrations/1727758667868_alter_users_table.ts
+++ database/migrations/1727758667868_alter_users_table.ts
@@ -5,8 +5,9 @@ export default class extends BaseSchema {
 
   async up() {
     this.schema.alterTable(this.tableName, (table) => {
-      table.string('password_reset_token', 128).nullable().unique()
-      table.timestamp('password_reset_token_expiry').nullable()
+      table.string('email_confirmation_token', 128).nullable().unique()
+      table.timestamp('email_confirmation_token_expiry').nullable()
+      table.boolean('is_email_confirmed').defaultTo(false)
     })
   }
 
