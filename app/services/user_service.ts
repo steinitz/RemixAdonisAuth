08e3ceb7050471708b4b1967467a850d5f6602cc
Steve Steinitz <steve@stzdev.com>
2024-10-02 14:59:26 +1000

    Add utility functions for email confirmation flow (refactored from password-reset token handling)

--- app/services/user_service.ts
+++ app/services/user_service.ts
@@ -1,11 +1,18 @@
 import User from '#models/user';
 import hash from '@adonisjs/core/services/hash';
-import { DateTime } from 'luxon'
+import {DateTime} from 'luxon'
 import string from "@adonisjs/core/helpers/string";
 // import db from '@adonisjs/lucid/services/db'
 
+
+type TokenType = 'password_reset_token' | 'email_confirmation_token'
+type TokenExpiryType = 'password_reset_token_expiry' | 'email_confirmation_token_expiry'
+
 export default class UserService {
-  async createUser(props: { email: string; password: string }) {
+
+  // User
+
+  async createUser(props: {email: string; password: string}) {
     return await User.create({
       email: props.email,
       password: props.password,
@@ -16,50 +23,31 @@ export default class UserService {
     return await User.findByOrFail('email', email)
   }
 
-  // password-reset token expires after 2 hours
-  tokenExpiryHours = 2
+  // Password Reset
 
-  // looks for user, via email, throws if not found
-  // generates a token
-  // sets the token field and expiry field
-  // returns a token
   async setPasswordResetTokenFor(email: string) {
-    const user = await this.getUser(email)
-    if (!user) {throw new Error('user not found')}
-    const token = string.generateRandom(64)
-    user.password_reset_token = token
-    user.password_reset_token_expiry = DateTime.local().plus({ hours: this.tokenExpiryHours })
-    user.save()
-
-    return token
-  }
-
-  tokenHasExpired(user: User | undefined) {
-    return user ?
-      user.password_reset_token_expiry
-        ? user.password_reset_token_expiry < DateTime.local()
-        : false
-      : true // return true if no user
+    return await this.setTokenFor(email, 'password_reset_token')
   }
 
   async getUserWithPasswordResetToken(
     token: string):
-    Promise< { user: User | undefined, tokenHasExpired: boolean }>
-  {
-    let user
-    try{
-      user = await User.findByOrFail('password_reset_token', token)
-     }
-    catch(error){
-      console.warn('no user found with specified password_reset_token', { token })
-    }
-
-    const tokenHasExpired = this.tokenHasExpired(user)
-    return {
-      user,
-      tokenHasExpired
-    }
+    Promise< {user: User | undefined, tokenHasExpired: boolean}> {
+    return this.getUserWithToken(token, 'password_reset_token')
   }
+
+ // Email Confirmation
+
+  async setEmailConfirmationTokenFor(email: string) {
+    return await this.setTokenFor(email, 'email_confirmation_token')
+  }
+
+  async getUserWithEmailConfirmationToken(
+    token: string):
+    Promise< {user: User | undefined, tokenHasExpired: boolean}> {
+    return this.getUserWithToken(token, 'email_confirmation_token')
+  }
+
+  // Password
 
   async updatePassword(user: User, password: string)
   {
@@ -78,7 +66,6 @@ export default class UserService {
     user.save()
   }
 
-
   async verifyPassword(user: User, password: string) {
     let userPassword = user.password
     if (!userPassword) {
@@ -87,4 +74,55 @@ export default class UserService {
     }
     return hash.verify(userPassword || '', password)
   }
+
+  // Token utilities
+
+  // Token expiry for password-reset AND email confirmation
+  tokenExpiryHours = 2
+
+  // looks for user, via email, throws if not found
+  // creates either a password-reset or email-confirmation token
+  // sets the relevant field and expiry field on the user
+  // returns the token
+  async setTokenFor(email: string, tokenType: TokenType) {
+    const user = await this.getUser(email)
+    if (!user) {throw new Error('user not found')}
+    const token = string.generateRandom(64)
+    user[tokenType] = token
+
+    const tokenExpiryField: TokenExpiryType = `${tokenType}_expiry`
+    user[tokenExpiryField] = DateTime
+      .local()
+      .plus({hours: this.tokenExpiryHours})
+
+    user.save()
+
+    return token
+  }
+
+  async getUserWithToken(
+    token: string,
+    tokenType: TokenType
+  ):
+    Promise< {user: User | undefined, tokenHasExpired: boolean}>{
+    let user
+    let tokenHasExpired = true
+
+    try {
+      user = await User.findByOrFail(tokenType, token) || {}
+      const tokenExpiryField: TokenExpiryType = `${tokenType}_expiry`
+      const tokenExpiry = user?.[tokenExpiryField]
+      tokenHasExpired = tokenExpiry ? tokenExpiry < DateTime.local() : true
+    }
+    catch(error){
+      console.warn(`no user found with specified ${tokenType}`, {token})
+    }
+
+    return {
+      user,
+      tokenHasExpired
+    }
+  }
+
+
 }
