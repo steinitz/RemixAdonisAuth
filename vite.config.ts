cc1a66f5b114b69807fe0cc149521f0dd4250aff
Steve Steinitz <steve@stzdev.com>
2024-10-20 01:18:06 +1100

    fix a rollup runtime error in production build by removing the esnext target option.  I added it a while back for a reason now forgotten.

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,9 @@ import {vitePlugin as remix} from '@remix-run/dev'
 import {defineConfig} from 'vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
-export default defineConfig(({ isSsrBuild }) => ({
+export default defineConfig(({ isSsrBuild }) => {
+  // console.log({isSsrBuild})
+  return {
   base: '/assets/',
   plugins: [
     remix({
@@ -13,12 +15,16 @@ export default defineConfig(({ isSsrBuild }) => ({
     tsconfigPaths(),
   ],
   optimizeDeps: {
-    esbuildOptions: isSsrBuild
-      ? {
-          target: 'ES2022',
-        }
+    esbuildOptions: isSsrBuild ?
+      {
+        target: 'ES2022',
+      }
+      // SJS - added esnext target for reasons forgotten but it gives
+      // a runtime error in a production build.  I'll keep it here
+      // for now in case I remember why I added it.
       : {
-        target: 'esnext', // SJS
+        // target: 'esnext',
+        target: 'ES2022',
       },
   },
-}))
+}})
