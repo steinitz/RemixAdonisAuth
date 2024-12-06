4888e4f8a5303097d95f33fb39fdf911dedea29b
Steve Steinitz <steve@stzdev.com>
2024-12-06 17:44:42 +1100

    put sqlite db filepath for test deployment into .env.production.local

--- config/database.ts
+++ config/database.ts
@@ -1,13 +1,19 @@
 import app from '@adonisjs/core/services/app'
 import { defineConfig } from '@adonisjs/lucid'
+import env from '#start/env'
 
+const nodeEnvironment = env.get('NODE_ENV') || 'development'
+const sqliteFilepath = env.get('SQLITE3_FILEPATH')
+console.log('database', {sqliteFilepath})
 const dbConfig = defineConfig({
   connection: 'sqlite',
   connections: {
     sqlite: {
       client: "better-sqlite3",
       connection: {
-        filename: app.tmpPath('db.sqlite3')
+        filename: nodeEnvironment === 'production' ?
+          sqliteFilepath ?? app.tmpPath('db.sqlite3') :
+          app.tmpPath('db.sqlite3')
       },
       useNullAsDefault: true,
       migrations: {
